e376ab042fd8b7179fe3742f90e96857e70e25eb
harshwardhan12 <lalitasolanki2577@gmail.com>
2025-11-27 22:22:01 +0530

    dom-task-spotlight

---
+++ JS-Tasks/Dom-task-9/script.js
@@ -0,0 +1,4 @@
+addEventListener("mousemove", function(dets) {
+    document.body.style.setProperty("--x", dets.clientX + "px");
+    document.body.style.setProperty("--y", dets.clientY + "px");
+})
