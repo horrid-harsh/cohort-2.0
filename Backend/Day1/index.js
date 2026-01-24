170464bcbb2c97d1814a8441f68eb52b424cd0e8
harshwardhan12 <lalitasolanki2577@gmail.com>
2026-01-24 17:20:15 +0530

    express-server

---
+++ Backend/Day1/index.js
@@ -0,0 +1,10 @@
+const catMe = require('cat-me');
+console.log(catMe());
+
+const express = require('express');
+app = express();
+
+app.listen(3000, ()=> {
+    console.log('server started');
+    
+})
