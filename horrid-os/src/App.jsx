f3e74a04d7bc490cd77cebf54e53f1fdeb57dd49
harshwardhan12 <lalitasolanki2577@gmail.com>
2026-01-25 16:23:35 +0530

    window component added

--- horrid-os/src/App.jsx
+++ horrid-os/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './app.scss'
 import Dock from './components/dock'
 import Navbar from './components/Navbar'
+import MacWindow from './components/windows/MacWindow'
 
 function App() {
 
@@ -10,6 +11,9 @@ function App() {
     <main>
       <Navbar />
       <Dock />
+      <MacWindow>
+        
+      </MacWindow>
     </main>
    </>
   )
---
+++ horrid-os/src/components/windows/MacWindow.jsx
@@ -0,0 +1,26 @@
+import { Rnd } from "react-rnd";
+import "./window.scss";
+
+const MacWindow = ({ children }) => {
+  return (
+    <Rnd>
+      <div className="window">
+        <div className="nav">
+          <div className="dots">
+            <div className="dot red"></div>
+            <div className="dot yellow"></div>
+            <div className="dot green"></div>
+          </div>
+          <div className="title">
+            <p>blackmyth - zsh</p>
+          </div>
+        </div>
+        <div className="main-content">
+            {children}
+        </div>
+      </div>
+    </Rnd>
+  );
+};
+
+export default MacWindow;
