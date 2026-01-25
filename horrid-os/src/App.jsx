0361fbd8bbdc2a54ec3e6191454a5773adb27f2c
harshwardhan12 <lalitasolanki2577@gmail.com>
2026-01-25 14:36:15 +0530

    dock component created

---
+++ horrid-os/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './app.scss'
+import Dock from './components/dock'
+
+function App() {
+
+  return (
+   <>
+    <main>
+      <Dock />
+    </main>
+   </>
+  )
+}
+
+export default App
