ad26db26a42e706b97aacf75217b9c7ee64e1601
harshwardhan12 <lalitasolanki2577@gmail.com>
2026-02-07 14:18:45 +0530

    JWT

---
+++ Backend/Day12/src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+function connectToDb() {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log('Connected to DB');        
+    })
+    .catch((err)=>{
+        console.log('Error: ', err);        
+    })
+}
+
+module.exports = connectToDb;
