79ede7eff66635a9041a58d8ea3a43263938ec9e
harshwardhan12 <lalitasolanki2577@gmail.com>
2026-02-17 17:46:53 +0530

    refactor(auth): move auth logic to middleware

--- Backend/Day14/src/controllers/post.controller.js
+++ Backend/Day14/src/controllers/post.controller.js
@@ -2,31 +2,18 @@ const postModel = require("../models/post.model");
 const userModel = require("../models/user.model");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
-const jwt = require("jsonwebtoken");
-const sharp = require('sharp');
+const sharp = require("sharp");
 
 const imageKit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 async function createPostController(req, res) {
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({
-      message: "Token not provided, Unauthorised access",
-    });
-  }
-
-  let decoded = null;
   try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
-
     const compressedBuffer = await sharp(req.file.buffer)
-    .resize({ width: 1080, withoutEnlargement: true })
-    .jpeg({ quality: 75 })
-    .toBuffer();
+      .resize({ width: 1080, withoutEnlargement: true })
+      .jpeg({ quality: 75 })
+      .toBuffer();
 
     const uploadResponse = await imageKit.files.upload({
       file: await toFile(compressedBuffer, "image.jpg"), // 👈 BUFFER USED HERE
@@ -39,91 +26,62 @@ async function createPostController(req, res) {
     const post = await postModel.create({
       caption: req.body.caption,
       imgUrl: uploadResponse.url,
-      user: decoded.id,
+      user: req.user.id,
     });
 
-    await userModel.findByIdAndUpdate(decoded.id, {
-      $push: {posts: post._id}
-    })
+    await userModel.findByIdAndUpdate(req.user.id, {
+      $push: { posts: post._id },
+    });
 
     return res.status(201).json({
       message: "post created successfully",
-      post
+      post,
     });
   } catch (error) {
     return res.status(401).json({
-      message: "User not authorized"
-    })
+      message: "User not authorized",
+    });
   }
 }
 
 async function getPostController(req, res) {
-  const token = req.cookies.token;
-
-  if(!token) return res.status(401).json({
-    message: 'Unauthorized Access'
-  });
-
-  let decoded = null;
-  try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
-  } catch (error) {
-    return res.status(401).json({
-      message: 'Invalid token'
-    });
-  }
-
-  const userId = decoded.id;
+  const userId = req.user.id;
   const posts = await postModel.find({
-    user: userId
+    user: userId,
   });
 
   return res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts
+    message: "Posts fetched successfully",
+    posts,
   });
 }
 
 async function getPostDetailsController(req, res) {
-  const token = req.cookies.token;
-  if(!token) return res.status(401).json({
-    message: 'Unauthorized Access'
-  });
-
-  let decoded = null;
-
-  try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
-  } catch (error) {
-    return res.status(401).json({
-      message: 'Invalid token'
-    });
-  }
-
-  const userId = decoded.id;
   const postId = req.params.postId;
+  const userId = req.user.id;
 
   const post = await postModel.findById(postId);
 
-  if(!post) return res.status(404).json({
-    mesage: 'Post not found'
-  });
+  if (!post)
+    return res.status(404).json({
+      mesage: "Post not found",
+    });
 
   const isValidUser = post.user.toString() === userId;
 
-  if(!isValidUser) return res.status(403).json({
-    message: 'Forbidden Content'
-  });
+  if (!isValidUser)
+    return res.status(403).json({
+      message: "Forbidden Content",
+    });
 
   return res.status(200).json({
-    message: 'Post fetched successully',
-    post
+    message: "Post fetched successully",
+    post,
   });
-
 }
 
 module.exports = {
   createPostController,
   getPostController,
-  getPostDetailsController
+  getPostDetailsController,
 };
--- Backend/Day14/src/routes/post.routes.js
+++ Backend/Day14/src/routes/post.routes.js
@@ -3,20 +3,22 @@ const postRouter = express.Router();
 const postController = require('../controllers/post.controller');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() })
+const authUser = require('../middlewares/auth.middleware');
 
 /**
  * POST /api/posts [protected]
  */
-postRouter.post('/', upload.single("image"), postController.createPostController);
+postRouter.post('/', authUser, upload.single("image"), postController.createPostController); // (upload.single("image")) Extracts the uploaded file from the "image" field and attaches it to req.file
+
 
 /**
  * GET /api/posts [protected]
  */
-postRouter.get('/', postController.getPostController);
+postRouter.get('/', authUser, postController.getPostController);
 
 /**
  * GET /api/posts/details/:postid
  */
-postRouter.get('/details/:postId', postController.getPostDetailsController)
+postRouter.get('/details/:postId', authUser, postController.getPostDetailsController)
 
 module.exports = postRouter;
